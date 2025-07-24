2bbfbfa82076909ca621770830ab5e6995772a7f
junghee111111 <jungheejjang1004@gmail.com>
2025-07-24 21:16:05 +0900

    wip: Spring Security Config

--- src/main/java/com/assslash/api/controller/AuthController.java
+++ src/main/java/com/assslash/api/controller/AuthController.java
@@ -5,12 +5,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@ResponseBody
 @RequestMapping("/auth")
 public class AuthController {
+
     @PostMapping("login")
     public ResponseEntity<String> getMemberProfile() {
         return ResponseEntity.status(HttpStatus.OK).body("OK");
