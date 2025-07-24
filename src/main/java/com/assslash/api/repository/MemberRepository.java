a340a2778731a1f8c25431fddc07121e282961db
junghee111111 <agent474@naver.com>
2025-07-24 13:28:55 +0900

    wip: impl. login with JWT token

---
+++ src/main/java/com/assslash/api/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package com.assslash.api.repository;
+
+import com.assslash.api.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
+}
--- src/main/java/com/assslash/api/security/config/SecurityConfig.java
+++ src/main/java/com/assslash/api/security/config/SecurityConfig.java
@@ -1,17 +1,32 @@
 package com.assslash.api.security.config;
 
+import com.assslash.api.security.filter.JwtAuthFilter;
+import com.assslash.api.security.handler.CustomAccessDeniedHandler;
+import com.assslash.api.security.service.CustomUserDetailsService;
+import com.assslash.api.util.JwtUtil;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@AllArgsConstructor
 public class SecurityConfig {
+
+    private final CustomUserDetailsService customUserDetailsService;
+    private final JwtUtil jwtUtil;
+    private final CustomAccessDeniedHandler accessDeniedHandler;
+    //private final CustomAuthenticationEntryPoint authenticationEntryPoint;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -21,18 +36,27 @@ public class SecurityConfig {
                 .cors((Customizer.withDefaults()))
                 .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(
                         SessionCreationPolicy.STATELESS
-                ))
-                .authorizeHttpRequests(auth -> auth
-                        // Swagger UI 관련 경로 허용
-                        .requestMatchers("/swagger-ui/**").permitAll()
-                        .requestMatchers("/swagger/**").permitAll()
-                        .requestMatchers("/v3/api-docs/**").permitAll()
-                        .requestMatchers("/swagger-ui.html").permitAll()
-                        .requestMatchers("/auth/login").permitAll()
-                        // 다른 모든 요청은 인증 필요
-                        .anyRequest().authenticated()
-                );
+                ));
 
+        http.formLogin(AbstractHttpConfigurer::disable);
+        http.httpBasic(AbstractHttpConfigurer::disable);
+
+        http.addFilterBefore(new JwtAuthFilter(customUserDetailsService, jwtUtil), UsernamePasswordAuthenticationFilter.class);
+        http.exceptionHandling(
+                (exceptionHandling) -> exceptionHandling
+                        .authenticationEntryPoint()
+                        .accessDeniedHandler(accessDeniedHandler)
+        );
+
+        http.authorizeHttpRequests(auth -> auth
+                // Swagger UI 관련 경로 허용
+                .requestMatchers("/swagger-ui/**").permitAll()
+                .requestMatchers("/swagger/**").permitAll()
+                .requestMatchers("/v3/api-docs/**").permitAll()
+                .requestMatchers("/swagger-ui.html").permitAll()
+                .requestMatchers("/auth/login").permitAll()
+                .anyRequest().permitAll()
+        );
         return http.build();
 
     }
